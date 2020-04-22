218b9bfc088698f7955c4d05e346fd5d6dc5fb7d
kjwan4435 <kjwan4435@gmail.com>
2020-04-23 03:13:46 +0900

    Component to relative location

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import styled from "styled-components";
-import Loader from "Components/Loader";
-import Section from "Components/Section";
-import Message from "Components/Message";
-import Poster from "Components/Poster";
+import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
+import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
 
 const Container = styled.div`
   padding: 20px;
